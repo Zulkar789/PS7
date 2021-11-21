cdbef32ab036600a91abf0bb0cf7793a0709ef4b
elmoun <zulelmoun6@gmail.com>
2021-11-22 02:19:48 +0800

    ok

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,36 +1,44 @@
-function mapSelect(data,text,value,targetId,defaultOpt){
-    data.forEach(function(itm){
-        $('<option>').val(itm[value]).text(itm[text]).appendTo('#'+targetId)
-        });
-    if(defaultOpt)
-        $('#'+targetId+'>option[value="'+defaultOpt+'"]').attr('selected', true);
+function mapSelect(data, text, value, targetId, defaultOpt) {
+  data.forEach(function (itm) {
+    $("<option>")
+      .val(itm[value])
+      .text(itm[text])
+      .appendTo("#" + targetId);
+  });
+  if (defaultOpt)
+    $("#" + targetId + '>option[value="' + defaultOpt + '"]').attr(
+      "selected",
+      true,
+    );
 }
-function removeSelect(targetId,defaultOption){
-    $('#'+targetId+' option').remove();
-    if(defaultOption)
-        $('<option>').text(defaultOption).appendTo('#'+targetId);
+function removeSelect(targetId, defaultOption) {
+  $("#" + targetId + " option").remove();
+  if (defaultOption)
+    $("<option>")
+      .text(defaultOption)
+      .appendTo("#" + targetId);
 }
-function onEnterKey(ev,fn){
-    if(ev.keyCode==13){
-        fn();
-    }
-    return true
+function onEnterKey(ev, fn) {
+  if (ev.keyCode == 13) {
+    fn();
+  }
+  return true;
 }
-function getFirstDayOfMonth(){
-    var curDate=new Date();
-    curDate.setTime(now.valueOf());
-    curDate.setDate(1);
-    curDate.setHours(0, 0, 0, 0);
-    return curDate;
+function getFirstDayOfMonth() {
+  var curDate = new Date();
+  curDate.setTime(now.valueOf());
+  curDate.setDate(1);
+  curDate.setHours(0, 0, 0, 0);
+  return curDate;
 }
-function getLastDayOfMonth(){
-    var curDate=getFirstDayOfMonth();
-    curDate.setMonth(curDate.getMonth()+1);
-    curDate.setTime(curDate.getTime()-1);
-    return curDate;
+function getLastDayOfMonth() {
+  var curDate = getFirstDayOfMonth();
+  curDate.setMonth(curDate.getMonth() + 1);
+  curDate.setTime(curDate.getTime() - 1);
+  return curDate;
 }
-function get30DaysDateFromNow(){
-    var curDate=new Date(now.getTime());
-    curDate.setDate(curDate.getDate()-29);
-    return curDate;
+function get30DaysDateFromNow() {
+  var curDate = new Date(now.getTime());
+  curDate.setDate(curDate.getDate() - 29);
+  return curDate;
 }
--- resources/tigra/menu_files/items1.js
+++ resources/tigra/menu_files/items1.js
@@ -1,40 +1,58 @@
-var MENU1_ITEMS =[
-	[wrap_blue('Home'), '/member/home', {'sw':80}],
-	
-	[wrap_blue('Account'), null, {'sw':80, 'bw':195},
-		['Edit Profil', '/member/edit-profile'],
-		['Bonus Account Statement', '/member/bonus-account-statement'],		
-		['Daftar Redemption', '/member/redemption-status'],
-		['Auto Maintenance Statement', '/member/auto-maintenance-statement'],	
-		['Registrasi Member Baru', '/member/register']
-	],
-	[wrap_blue('Bonus'), null, {'sw':80, 'bw':180},
-		['Bonus Harian', '/member/bonus-daily'],
-		['Bonus Mingguan', '/member/bonus-weekly'],
-		['Bonus Bulanan', '/member/bonus-monthly'],		
-	],
-	[wrap_blue('Report'), null, {'sw':80, 'bw':210},
-		['Susunan Jaringan (Genealogi)', '/member/genealogy'],
-		['Laporan Tutup Poin', '/member/leadership-tree'],
-		['Laporan Perkembangan Jaringan', '/member/network-performance-report'],
-		['Laporan Pembelanjaan Pribadi', '/member/history-personal-sales'],
-                ['Daftar Sponsor Langsung', '/member/list-direct-sponsor?list1=1'],
-	],
-	
-	[wrap_blue('Utility'), null, { 'sw':80, 'bw':150},
-		['Ganti Password', '/member/change-password'],
-		['Download', '/member/download']
-	],
-	[wrap_blue('Sign Out'), '/member/logout', {'sw':80}]
+var MENU1_ITEMS = [
+  [wrap_blue("Home"), "/member/home", { sw: 80 }],
+
+  [
+    wrap_blue("Account"),
+    null,
+    { sw: 80, bw: 195 },
+    ["Edit Profil", "/member/edit-profile"],
+    ["Bonus Account Statement", "/member/bonus-account-statement"],
+    ["Daftar Redemption", "/member/redemption-status"],
+    ["Auto Maintenance Statement", "/member/auto-maintenance-statement"],
+    ["Registrasi Member Baru", "/member/register"],
+  ],
+  [
+    wrap_blue("Bonus"),
+    null,
+    { sw: 80, bw: 180 },
+    ["Bonus Harian", "/member/bonus-daily"],
+    ["Bonus Mingguan", "/member/bonus-weekly"],
+    ["Bonus Bulanan", "/member/bonus-monthly"],
+  ],
+  [
+    wrap_blue("Report"),
+    null,
+    { sw: 80, bw: 210 },
+    ["Susunan Jaringan (Genealogi)", "/member/genealogy"],
+    ["Laporan Tutup Poin", "/member/leadership-tree"],
+    ["Laporan Perkembangan Jaringan", "/member/network-performance-report"],
+    ["Laporan Pembelanjaan Pribadi", "/member/history-personal-sales"],
+    ["Daftar Sponsor Langsung", "/member/list-direct-sponsor?list1=1"],
+  ],
+
+  [
+    wrap_blue("Utility"),
+    null,
+    { sw: 80, bw: 150 },
+    ["Ganti Password", "/member/change-password"],
+    ["Download", "/member/download"],
+  ],
+  [wrap_blue("Sign Out"), "/member/logout", { sw: 80 }],
 ];
 
-function wrap_blue (text) {
-	var res=[];
-	for (var i=0; i<3; i++)
-		res[i]=['<table cellpadding="0" cellspacing="0" border="0"><tr><td><img src="/resources/images/blue',
-			(i?2:1),'_l.gif" width="8" height="21" border="0"></td><td background="/resources/images/blue',
-			(i?2:1),'_m.gif" style="font-face: tahoma, verdana, arial; font-size: 11px; text-align:center; font-weight: bold; color: #ffffff" width="100%">',text,'</td><td><img src="/resources/images/blue',
-			(i?2:1),'_r.gif" width="8" height="21" border="0"></td></tr></table>'
-		].join('');
-	return res;
+function wrap_blue(text) {
+  var res = [];
+  for (var i = 0; i < 3; i++)
+    res[i] = [
+      '<table cellpadding="0" cellspacing="0" border="0"><tr><td><img src="/resources/images/blue',
+      i ? 2 : 1,
+      '_l.gif" width="8" height="21" border="0"></td><td background="/resources/images/blue',
+      i ? 2 : 1,
+      '_m.gif" style="font-face: tahoma, verdana, arial; font-size: 11px; text-align:center; font-weight: bold; color: #ffffff" width="100%">',
+      text,
+      '</td><td><img src="/resources/images/blue',
+      i ? 2 : 1,
+      '_r.gif" width="8" height="21" border="0"></td></tr></table>',
+    ].join("");
+  return res;
 }
